31194c08e1108ac64ff3069cde4b2f7293800b2d
mehmeterengunduz6 <mehmeterengunduz6@gmail.com>
2024-12-25 16:40:21 +0300

    responsive-fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './tailwind.css';
 
 const App = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white'>
-      <h1>Editable Map</h1>
-      <EditableMap />
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white p-4'>
+      <h1 className="text-2xl font-bold mb-4">Hakim Savcı Atama Simülasyonu</h1>
+      <div className="w-full max-w-[1200px]">
+        <EditableMap />
+      </div>
     </div>
   );
 };
